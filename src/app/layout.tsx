312c71cc02a566860a50722e0d47cea2960a241f
MediaZ DEV <116047433+trungducnet123@users.noreply.github.com>
2024-08-12 14:44:51 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Draw the I Ching and predict the future - Kabastro',
+  title: 'Draw the I Ching and predict the future by Kabastro',
   description: 'Useful tool to help you learn about I Ching as well as approach the Eastern philosophical system',
 };
 
